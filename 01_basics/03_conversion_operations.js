351e0118b2672f65f2d1949fd44eaf9ac125e899
Abhijeet Raj <abhijeetoo505@gmail.com>
2025-01-01 14:53:34 +0000

    Conversion to strings

--- 01_basics/03_conversion_operations.js
+++ 01_basics/03_conversion_operations.js
@@ -17,8 +17,16 @@ true => 1 and false => 0
 let isLoggedin = "Abhijeet"
 
 let booleanLoggedin = Boolean(isLoggedin)
-console.log(booleanLoggedin);
+//console.log(booleanLoggedin);
+
 
 // 1 or any number → true and 0 → false
 // ""[empty string] → false
 // "Abhijeet"[non empty string] → true
+
+let num = 45
+
+let str = String(num)
+
+console.log(str);
+console.log(typeof str);
