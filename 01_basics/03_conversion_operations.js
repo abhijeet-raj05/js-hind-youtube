9472269e6759fb5f7ae49b772a06cf7563308a94
Abhijeet Raj <abhijeetoo505@gmail.com>
2025-01-01 14:50:56 +0000

    Conversion Of datatypes

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,24 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+
+// const MyNumber = Number(score)
+// console.log(typeof MyNumber);
+// console.log(MyNumber);
+
+/*
+"33" => converted to number 33
+"33abc" => NaN [Not a number] or for any string
+true => 1 and false => 0
+
+
+*/
+
+let isLoggedin = "Abhijeet"
+
+let booleanLoggedin = Boolean(isLoggedin)
+console.log(booleanLoggedin);
+
+// 1 or any number → true and 0 → false
+// ""[empty string] → false
+// "Abhijeet"[non empty string] → true
