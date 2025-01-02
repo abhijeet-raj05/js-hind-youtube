e12c3287cf84e83a19ef5edc2586034b8f3e8543
Abhijeet Raj <abhijeetoo505@gmail.com>
2025-01-02 13:57:28 +0000

    Summary

--- 01_basics/05_Summary.js
+++ 01_basics/05_Summary.js
@@ -8,10 +8,48 @@
 
 //JavaScript is a dynamically typed language. Because data type will automatically assigned at the time of compilation or code execution.
 
+/*
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/ 
 const id_01 = Symbol('1234')
 const id_02 = Symbol('1234')
 
-console.log(id_01);
-console.log(id_02);
+// console.log(id_01);
+// console.log(id_02);
 
 console.log(id_01 === id_02);
+
+const bigNumber = 46584123846513279846513n
+
+console.log(bigNumber);
+
+const heros = ["Shaktimaan","Superman","Batman"] //arrays
+
+//object: 
+
+let myObject = {
+       name: "Abhijeet",
+       age: 18
+}
+// console.log(myObject);
+
+const myFunction = function(){
+       console.log("Hello World");
+       
+}
+
+console.log(typeof myFunction);
+
